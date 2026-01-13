08d3bdfb30068086b20b0492dd5b3e79ae26a070
Nenad Cvetkovic <nenad.cvetkovic@hotmail.com>
2026-01-13 17:19:31 +0100

    Improving a call to dotenv

--- playwright.config.ts
+++ playwright.config.ts
@@ -6,7 +6,11 @@ import { defineConfig, devices } from '@playwright/test';
  */
 import dotenv from 'dotenv';
 import path from 'path';
-dotenv.config({ path: path.resolve(__dirname, '.env') });
+
+if (!process.env.CI) {
+  dotenv.config({ path: path.resolve(__dirname, '.env') });
+}
+
 
 /**
  * See https://playwright.dev/docs/test-configuration.
