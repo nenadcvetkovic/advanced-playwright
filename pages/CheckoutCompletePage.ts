1f976a7b694f5e39c35d0f8b89131b2b0d33e239
Nenad Cvetkovic <nenad.cvetkovic@hotmail.com>
2026-01-14 17:00:22 +0100

    Trying projetc flows

--- pages/CheckoutCompletePage.ts
+++ pages/CheckoutCompletePage.ts
@@ -2,18 +2,13 @@ import { Page } from "@playwright/test"
 import { HeaderComponent } from "../components/HeaderComponent";
 
 export class CheckoutCompletePage {
-
     public header: HeaderComponent
-
 
     constructor(private readonly page: Page) {
         this.header = new HeaderComponent(this.page.locator('header.container'));
     }
 
     async getMessage(): Promise<string> {
-
         return await this.page.locator('[data-testid="page.checkout.complete"] p').textContent() as string;
-
     }
-
 }
